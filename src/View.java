ebbda9a9cebc3ec45dad89791ac35ca03e704648
ceisman21 <ceisman21@kentdenver.org>
2021-02-05 18:09:38 -0700

    View Class Work

--- src/View.java
+++ src/View.java
@@ -2,9 +2,45 @@ import javax.swing.*;
 import java.awt.*;
 
 public class View {
-    JFrame viewPort;
+    //Frame Instance. Populated by View();
+    protected JFrame viewPort;
+    protected GraphicsConfiguration gConfig;
+
+    //Find Bounds Instance. Populated by initViews()
+    protected Rectangle virtualBounds;
+    protected GraphicsEnvironment ge;
+    protected GraphicsDevice[] gs;
+    protected GraphicsConfiguration[] gc;
 
     public View() {
+        //Initializes Views
+        initViews();
+
+        //Sets up the ViewPort
+        viewPort = new JFrame("HOI4 Mod Maker - By Cooper Eisman", gConfig);
+    }
+
+    //Calculates the Bounds of the Device, and sets the instance to match;
+    private void initViews() {
+        //Set Variables
+        virtualBounds = new Rectangle();
+        ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        gs = ge.getScreenDevices();
+
+        //Calculates the size of every view into virtualBounds
+        for (int j = 0; j < gs.length; j++) {
+            GraphicsDevice gd = gs[j];
+            gc = gd.getConfigurations();
+
+            for (int i=0; i < gc.length; i++) {
+                virtualBounds = virtualBounds.union(gc[i].getBounds());
+            }
+        }
+    }
+
+
+
+    /*
         Runtime runtime = Runtime.getRuntime();
 
         JPanel mainMenu = new JPanel();
@@ -28,7 +64,7 @@ public class View {
         viewPort.setVisible(true);
 
 
-        /* This is for messing with runtimes
+        This is for messing with runtimes
         try {
             synchronized(Main.main) {
                 Main.main.wait(1000);
@@ -37,8 +73,4 @@ public class View {
             System.exit(0);
         }
         */
-
-
-
-    }
 }
