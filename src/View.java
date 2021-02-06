86a77c37704392c2c8e0e2a6b12deedbdc55b3c2
ceisman21 <ceisman21@kentdenver.org>
2021-02-05 19:42:55 -0700

    Fixed FullScreen Bug

--- src/View.java
+++ src/View.java
@@ -6,55 +6,73 @@ Last Modified: 02/05/2021
 
 import javax.swing.*;
 import java.awt.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
 public class View {
-    //Frame Instance. Populated by View();
+    //Frame Instance Vars
     protected JFrame viewPort;
     protected GraphicsConfiguration gConfig;
-
-    //Find Bounds Instance. Populated by initViews()
-    protected Rectangle virtualBounds;
     protected GraphicsEnvironment ge;
-    protected GraphicsDevice[] gs;
-    protected GraphicsConfiguration[] gc;
+    protected GraphicsDevice[] gd;
 
     //Constructor
     public View() {
-        //Initializes Views
-        initViews();
+        //Finds all the Views and Populates Instance
+        ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        gd = ge.getScreenDevices();
+        gConfig = ge.getDefaultScreenDevice().getConfigurations()[0];
+
 
         //Sets up the ViewPort
-        viewPort = new JFrame("HOI4 Mod Maker - By Cooper Eisman", gConfig);
-        viewPort.setMaximizedBounds(virtualBounds);
-        viewPort.setSize(gc[0].getBounds().getSize());
+        viewPort = new JFrame("HOI4 Mod Maker - By Cooper Eisman");
+
+        //Sets Size and Behaviour
+        viewPort.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        viewPort.setSize(gConfig.getBounds().getSize());
+        viewPort.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        //Set ViewPort Bounds
+        viewPort.setMinimumSize(new Dimension((int)(gConfig.getBounds().getX()/4), (int)(gConfig.getBounds().getX()/4)));
+        viewPort.setMaximizedBounds(gConfig.getBounds());
+        viewPort.setMaximumSize(gConfig.getBounds().getSize());
+
+        //Adds listener to avoid overstepping view bounds
+        addEventListener();
+
         viewPort.setVisible(true);
     }
 
-    //Calculates the Bounds of the Device, and sets the instance to match;
-    private void initViews() {
-        //Set Variables
-        virtualBounds = new Rectangle();
-        ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-        gs = ge.getScreenDevices();
+    //Sets up Movement so that the App will not Exceed maximum frame size
+    protected void addEventListener() {
+        //Add a Listener for if Properties Change
+        PropertyChangeListener viewListener = new PropertyChangeListener() {
+            @Override
+            public void propertyChange(PropertyChangeEvent evt) {
+                //Update Sizes whenever position changes
+                gConfig = viewPort.getGraphicsConfiguration();
 
-        //Calculates the size of every view into virtualBounds
-        for (int j = 0; j < gs.length; j++) {
-            GraphicsDevice gd = gs[j];
-            gc = gd.getConfigurations();
+                viewPort.setMaximizedBounds(gConfig.getBounds());
+                viewPort.setMaximumSize(gConfig.getBounds().getSize());
 
-            for (int i=0; i < gc.length; i++) {
-                virtualBounds = virtualBounds.union(gc[i].getBounds());
+                //ensures no window escapes minimum size
+                if (viewPort.getOwnedWindows().length != 0) {
+                    viewPort.getOwnedWindows()[0].setMinimumSize(new Dimension((int)(gConfig.getBounds().getX()/4), (int)(gConfig.getBounds().getX()/4)));
+                }
             }
-        }
+        };
+
+
+        viewPort.addPropertyChangeListener(viewListener);
     }
 
     //Returns the Max Height of the Frame
     public int getMaxHeight() {
-        return virtualBounds.getSize().height;
+        return viewPort.getMaximumSize().height;
     }
 
     //Returns the max width of the Frame
     public int getMaxWidth() {
-        return virtualBounds.getSize().width;
+        return viewPort.getMaximumSize().width;
     }
 }
