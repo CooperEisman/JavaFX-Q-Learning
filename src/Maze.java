eae0ea343f0a4fd77b14526f00678594188d4e9e
CooperEisman <cooper@eisman.me>
2021-02-08 11:33:06 -0700

    Initial Commit

---
+++ src/Maze.java
@@ -0,0 +1,15 @@
+//Cooper Eisman -- Maze Class
+
+import java.io.*;
+
+
+public class Maze {
+    //Instance
+    private int width;
+    private int height;
+    private File maze;
+
+    public Maze() {
+
+    }
+}
