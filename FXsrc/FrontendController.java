c2ffdafede4473735aae33de34b0fcaae0bc33b2
ceisman21 <ceisman21@kentdenver.org>
2021-02-08 21:03:12 -0700

    More Work on VCisual

--- FXsrc/FrontendController.java
+++ FXsrc/FrontendController.java
@@ -13,11 +13,20 @@ public class FrontendController {
     private View view;
     private JPanel currScreen;
     private LayoutManager layoutManager;
+
     private JPanel south;
     private JPanel east;
     private JPanel center;
 
+    private int width;
+    private int height;
+
     public FrontendController(int width, int height) {
+        //Initiate Input
+        this.width = width;
+        this.height = height;
+
+
         //Itialize the View and Layout
         view = new View();
         layoutManager = new BorderLayout();
@@ -27,19 +36,17 @@ public class FrontendController {
         currScreen.setLayout(layoutManager);
         loadLayout();
 
-
-
-        currScreen.validate();
-
-
+        //Initial View
         loadScreen();
     }
 
     public void loadScreen() {
+        currScreen.validate();
         view.configureViewPort(currScreen);
     }
 
-    private void loadLayout(int[][] vars) {
+    //Loads the Touchy THings
+    private void loadLayout() {
         //South Frame
         south = new JPanel();
         south.setMinimumSize(new Dimension(view.getMaxWidth(),(view.getMaxHeight()/5)*2));
@@ -52,8 +59,34 @@ public class FrontendController {
 
         //Center Frame
         center = new JPanel();
+        center.setLayout(new GridLayout(width, height));
         center.setMinimumSize(new Dimension((view.getMaxWidth()/5)*4,(view.getMaxHeight()/5)*3));
         currScreen.add(center);
+    }
+
+    //Load the Screen
+    public void updateCenter(int[][] vars) {
+        center.removeAll();
+        center.setLayout(new GridLayout(width, height));
+        for(int x = 0; x < vars.length; x++) {
+            for(int y = 0; x < vars[x].length; y++) {
+                Button b;
+                if (vars[x][y] == -1) {
+                    b = new Button("Barrier");
+                    b.setBackground(new Color(0));
+                } else if (vars[x][y] == 1) {
+                    b = new Button("Start");
+                    b.setBackground(new Color(101010));
+                } else if (vars[x][y] == 10) {
+                    b = new Button("Finish");
+                    b.setBackground(new Color(100100100));
+                } else {
+                    b = new Button("Box");
+                }
+                center.add(b);
+            }
+        }
+        loadScreen();
     }
 
 
